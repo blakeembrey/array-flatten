4f064f21201f27b5bee56b937b3b7342f76a7661
Deema Yv <df.creative@gmail.com>
2016-05-22 15:17:15 -0400

    Fix codestandard

--- array-flatten.js
+++ array-flatten.js
@@ -15,7 +15,9 @@ module.exports.fromDepth = flattenFromDepth
  * @return {Array}
  */
 function flatten (array) {
-  if (ArrayBuffer.isView(array)) return array;
+  if (ArrayBuffer.isView(array)) {
+    return array
+  }
   
   if (!Array.isArray(array)) {
     throw new TypeError('Expected value to be an array')
